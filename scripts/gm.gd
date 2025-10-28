dd6e814b763681edfd304bd53fc2f9cc37d0ccd7
profernandosirianni <profernandosirianni@gmail.com>
2025-10-28 18:03:40 -0300

    Ajustes dos efeitos de fade in para inicio de cada cena
    Ajuste feito no process ready do script world

--- scripts/gm.gd
+++ scripts/gm.gd
@@ -4,9 +4,12 @@ var cenamenu = false
 var podemovimentar = true
 var CutToWorld = false
 var Cut2ToWorld2 = false
-
+var endsceneworld = false
 func _process(delta: float) -> void:
 	#print(podemovimentar)
+	
+	# Imprime o nome do nó da cena atual.
+	#print(get_tree().current_scene.scene_file_path)
 	
 	
 	if Cut2ToWorld2 == true:
--- scripts/world.gd
+++ scripts/world.gd
@@ -9,39 +9,47 @@ var player
 var estanotronco = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	anim.play("fade_in")
+	if scene_file_path == "res://cenas/cutscene.tscn":
+		anim.play("fade_in")
+	if scene_file_path == "res://cenas/world.tscn":
+		anim.play("fade_in")
+	if scene_file_path == "res://cenas/cutscene2.tscn":
+		anim.play("fade_in")
 	player = get_node("player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
+
+		
 	if interacao == true and Input.is_action_just_pressed("interact"):
 		
-		tronco.play("TroncoComGato")
+		tronco.play("TroncoComGato") #muda para a camera na mesma cena para o gato dentro de um tronco
 		#await get_tree().create_timer(1.0).timeout
-		
+		#desabilita a movimentação e outros controles (como texto de interaçao) assim que o gato fica dentro do tronco
 		Gm.podemovimentar = false
 		textoInteracao.visible = false
 		player.visible = false
 		camera.zoom = Vector2(2,2)
-		estanotronco = true
-		await get_tree().create_timer(3.0).timeout
-		anim.play("fade_in")
-		await get_tree().create_timer(1.0).timeout
+		estanotronco = true #variavel de estado para guardar um estado
+		await get_tree().create_timer(3.0).timeout # temporizador para nada ocorrer por 3 segundos enquanto o gato fica dentro do tronco
+		Gm.endsceneworld = true #envia um sinal para o game manager mudando o estado da situação da cena
+		await get_tree().create_timer(1.0).timeout #um temporizador de 1 segundo para durar a animação de fade out antes de mudar para a proxima cena
 		get_tree().change_scene_to_file("res://cenas/cutscene2.tscn")
+		player.position = Vector2(1040, -70) #muda o player de posição alem de deixar invisivel (linha 26)
+	
+	if Gm.CutToWorld == true:
 		
-	#if estanotronco == true and Input.is_action_just_pressed("interact"):
-		#print("Foi")
+		anim.play("fade_out")
+		await get_tree().create_timer(1.0).timeout
+		get_tree().change_scene_to_file("res://cenas/world.tscn")
+		Gm.CutToWorld = false
 		
-		#player.position = Vector2(1040, -70)
-	#if Gm.CutToWorld == true:
-		#
-		#anim.play("fade_out")
-		#await get_tree().create_timer(1.0).timeout
-		#Gm.CutToWorld = false
-		#get_tree().change_scene_to_file("res://cenas/world.tscn")
 		
+	#analisa a situação da cena de acordo com o sinal enviado ao GM
+	if Gm.endsceneworld == true:
+		anim.play("fade_out") #A cena atual deve escurecer para então entrar a proxima cena (mas parece que a animação que roda é a fade in)
+		Gm.endsceneworld = false #reseta o sinal
 		
 func _on_exit_pressed() -> void:
 	get_tree().quit()
@@ -59,9 +67,6 @@ func _on_interacao_body_exited(_body: Node2D) -> void:
 	textoInteracao.visible = false
 	interacao = false
 
-
-func _on_animation_player_animation_started(anim_name: StringName) -> void:
-	anim.play("fade_in")  # toca a animação
 
 
 func _on_timer_timeout() -> void:
